a67c84c068cf07e8c18d870deb8fb535da714ed3
alexa-whitney <alexawhitney@gmail.com>
2023-06-12 15:30:36 -0600

    Added broadcast message when user connects or disconnects

--- index.js
+++ index.js
@@ -13,17 +13,30 @@ app.get('/', (req, res) => {
 const users = {};
 
 io.on('connection', (socket) => {
+    socket.on('set nickname', (nickname) => {
+        users[socket.id] = nickname;
+
+        // Broadcast a message when someone connects
+        socket.broadcast.emit('chat message', {
+            nickname: 'System',
+            message: `${nickname} has joined the chat`
+        });
+    });
+
     socket.on('chat message', (data) => {
         const { nickname, message } = data;
         io.emit('chat message', { nickname, message });
     });
 
-    socket.on('set nickname', (nickname) => {
-        users[socket.id] = nickname;
-    });
-
     socket.on('disconnect', () => {
+        const nickname = users[socket.id];
         delete users[socket.id];
+
+        // Broadcast a message when someone disconnects
+        socket.broadcast.emit('chat message', {
+            nickname: 'System',
+            message: `${nickname} has left the chat`
+        });
     });
 });
 
